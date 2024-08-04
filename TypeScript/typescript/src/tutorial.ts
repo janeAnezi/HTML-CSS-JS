854d0cc0ecd8c268639cc566791ccbbc4ae182b9
Anezijane <ahnnieanezi@gmail.com>
2024-08-04 16:55:13 +0100

    initial commit

---
+++ TypeScript/typescript/src/tutorial.ts
@@ -0,0 +1,25 @@
+// Type Anotations
+let awesomeName: string = 'shakeAndBake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 12 - 1
+
+let isAwesome: boolean = true
+isAwesome = false
+
+// ## Challenge 1
+let fullName: string = 'Jane Anezi'
+fullName = fullName.toUpperCase(); 
+
+let age: number = 25
+age = age - 5
+
+let isAdult: boolean = true
+isAdult = false
+if (!isAdult) {
+    console.log("great job!");    
+}
+//isCorrect = 'no'
---
+++ projects/autoTextAnimation/text.js
@@ -0,0 +1,69 @@
+let containerEl = document.querySelector('.container');
+// containerEl.style.backgroundColor = 'yellow';
+
+
+const careers = ['Software developer', 'Web Developer', 'Freelancer', 'Instructor'];
+
+
+
+let careerIndex = 0;
+console.log(careerIndex, careers.length);
+// console.log(careers[careerIndex].length);
+let characterIndex = 0;
+
+updateCareers()
+
+function updateCareers() {
+    characterIndex++;
+    containerEl.innerHTML = `<h1> I am a ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+   
+    
+    if ( characterIndex === careers[careerIndex].length ) {
+        careerIndex++;
+        characterIndex = 0
+    }
+
+    if (careerIndex === careers.length) {
+        careerIndex = 0;
+    }
+
+    setTimeout(updateCareers, 300);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let careerIndex = 0;
+
+// let characterIndex = 0;
+
+// updateText()
+
+// function updateText() {
+//     characterIndex++;
+//     containerEl.innerHTML = `<h1> I am ${careers[careerIndex].slice(0,1)=== 'I' ? 'an' : 'a'} ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+   
+
+//     if ( characterIndex === careers[careerIndex].length ) {
+//         careerIndex++;
+//         characterIndex = 0
+//     }
+
+//     if (careerIndex === careers.length) {
+//         careerIndex = 0;
+//     }
+
+//     setTimeout(updateText, 300);
+// }
+
